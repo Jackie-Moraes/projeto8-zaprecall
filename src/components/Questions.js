9b772a416259bd13977a950d749d1fb87ef0218e
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-03-18 14:46:02 -0300

    Perguntas enumeradas e dinâmicas

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -2,29 +2,64 @@ import React from "react";
 
 export default function Questions() {
 
-    const perguntas = ["O que é JSX?", "O React é __", "Componentes devem iniciar com __", "Podemos colocar __ dentro do JSX", "O ReactDOM nos ajuda __", "Usamos o npm para __", "Usamos props para __", "Usamos estado (state) para __"]
+    const perguntas = [{
+        pergunta: "O que é JSX?",
+        resposta: "JSX é uma sintaxe para escrever HTML dentro do JS"
+    },
+    {
+        pergunta: "O React é __",
+        resposta:"uma biblioteca JavaScript para construção de interfaces"
+    },
+    {
+        pergunta: "Componentes devem iniciar com __",
+        resposta:"letra maiúscula"
+    },
+    {
+        pergunta: "Podemos colocar __ dentro do JSX",
+        resposta:"expressões"
+    },
+    {
+        pergunta: "O ReactDOM nos ajuda __",
+        resposta:"interagindo com a DOM para colocar componentes React na mesma"
+    },
+    {
+        pergunta: "Usamos o npm para __",
+        resposta:"gerenciar os pacotes necessários e suas dependências"
+    },
+    {
+        pergunta: "Usamos props para __",
+        resposta:"passar diferentes informações para componentes"
+    },
+    {
+        pergunta: "Usamos estado (state) para __",
+        resposta:"dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
+    }];
 
     const [pergunta, setPergunta] = React.useState(true);
     const [questao, setQuestao] = React.useState(false);
     const [resposta, setResposta] = React.useState(false);
 
-    function Pergunta() {
+    function Pergunta(props) {
 
         return (
-            <div className="question-closed">
-                <h2>Pergunta 1</h2>
-                <img src="images/setinha.svg" alt="Abrir pergunta" onClick={() => 
-                    {setPergunta(false) 
-                        setQuestao(true)}}>
-                    </img>
+            <div className="question">
+                {pergunta ? <div className="question-closed">
+                    <h2>Pergunta {props.numero}</h2>
+                    <img src="images/setinha.svg" alt="Abrir pergunta" onClick={() => 
+                        {setPergunta(false) 
+                            setQuestao(true)}}>
+                        </img>
+                </div> : <></>}
+                {questao ? <Questao pergunta={props.pergunta}/> : <></>}
+                {resposta ? <Resposta resposta={props.resposta}/> : <></>}
             </div>
         )
     }
     
-    function Questao() {
+    function Questao(props) {
         return (
             <div className="question-open">
-                <h2>O que é JSX?</h2>
+                <h2>{props.pergunta}</h2>
                 <img src="images/setinha-volta.png" alt="Virar Card" onClick={() =>
                     {setQuestao(false)
                         setResposta(true)}}>
@@ -33,10 +68,10 @@ export default function Questions() {
         )
     }
     
-    function Resposta() {
+    function Resposta(props) {
         return (
             <div className="question-open">
-                <h2>JSX é uma sintaxe para escrever HTML dentro do JS</h2>
+                <h2>{props.resposta}</h2>
                 <div className="answers">
                     <button className="red" onClick={() => {
                         setResposta(false)
@@ -55,11 +90,7 @@ export default function Questions() {
 
     return (
         <section className="questions">
-            <div className="question">
-                {pergunta ? <Pergunta /> : <></>}
-                {questao ? <Questao /> : <></>}
-                {resposta ? <Resposta /> : <></>}
-            </div>
+            {perguntas.map((p, index) => <Pergunta numero={index+1} pergunta={p.pergunta} resposta={p.resposta}/>)}
         </section>
    )
 }
